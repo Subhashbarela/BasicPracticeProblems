5b7f60ee99b68eef912a91ca83b394dbdfee4a80
Subhash Barela <barelasubhash16@gmail.com>
2023-04-25 21:54:24 +0530

    Check two number are equal or not

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConceptPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckEquality eql= new CheckEquality();
+            eql.CheckEqualOrNot();
+                Console.ReadLine();
+        }
+    }
+}
